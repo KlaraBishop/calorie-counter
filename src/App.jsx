2e76131277635a22d51af472e9e1771d26901901
KleraStretch <stretch068@protonmail.com>
2023-04-12 21:17:44 -0700

    Added initial parts of the project, not working yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,58 @@
 import './stylesheets/App.css'
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">Hewwo World</div>
-  )
+  const [itemText, setItemText] = useState('');
+  const [itemCalories, setItemCalories] = useState('');
+
+  const [itemList, setItemList] = useState([{name:"Cookie", calories: 270}]);
+  const [total, setTotal] = useState(() => {
+    return itemList.reduce((acc, o) => acc + o.calories, 0)
+  })
+
+  const onItemUpdate = (e) => {
+    setItemText(() => e.target.value)
+  }
+
+  const onCaloriesUpdate = (e) => {
+    setItemCalories(() => e.target.value)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    setItemList(() => {
+      return itemList.concat({name:{itemText}, calories:{itemCalories}})
+    })
+  }
+
+  return <>
+    <h2>Total Calories</h2>
+    <h2>{total}</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>Calories</th>
+        </tr>
+      </thead>
+      <tbody>        
+        {itemList.map(item => {
+          return <tr key={item.name}>
+            <td>{item.name}</td>
+            <td>{item.calories}</td>
+          </tr>
+        })}
+      </tbody>
+    </table>
+    <form>
+      <label htmlFor='item'>Item: </label>
+      <input type="text" id='item' name='item' value={itemText} onChange={onItemUpdate} />
+      <label htmlFor='calories'>Calories: </label>
+      <input type="text" id='calories' name='calories' onChange={onCaloriesUpdate} />
+      <button type="submit" onClick={onSubmit}>Add</button>
+    </form>
+  </>
 }
 
 export default App
